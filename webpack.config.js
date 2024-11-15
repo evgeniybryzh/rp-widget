9b856da26d7010654dd9472740850a260e3d8a4d
evgeniybryzh <evgeniy@merge.rocks>
2024-11-15 10:35:52 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx", // Specify TypeScript entry point
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"], // Add .tsx and .ts extensions
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // Match .ts and .tsx files
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(js|jsx)$/, // Match .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // Match styles
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // Match images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
