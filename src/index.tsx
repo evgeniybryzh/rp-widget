22fd99a5eb20097133338147b4a5a09a3f1aeb2c
evgeniybryzh <evgeniy@merge.rocks>
2024-12-02 10:40:44 +0200

    Code refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const container = document.getElementById("rp-widget");
-if (container) {
-  const root = createRoot(container);
-  const isWebflow = container.hasAttribute("data-is-webflow");
-  root.render(<App isWebflow={isWebflow} />);
-} else {
-  console.error("Container element not found");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("rp-widget");
+
+  if (container) {
+    const root = createRoot(container);
+    const isWebflow = container.hasAttribute("data-is-webflow");
+    root.render(<App isWebflow={isWebflow} />);
+  } else {
+    console.error("Container element not found");
+  }
+});
